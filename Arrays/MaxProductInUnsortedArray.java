dee9a506e5fb519595fe2d0d82c2db4b852e6b25
AkshataABhat <74967139+AkshataABhat@users.noreply.github.com>
2021-10-15 01:00:56 +0530

    Update MaxProductInUnsortedArray.java

--- Arrays/MaxProductInUnsortedArray.java
+++ Arrays/MaxProductInUnsortedArray.java
@@ -5,32 +5,25 @@
 3. a. array len <2=> -1
    b. array len =2 => mul of those 2
 4.  condition for smallest
-    condition for secsmallest
-    
+    condition for secsmallest    
 
     cond for greatest
     cond for secgreat
 
-
-
     for loop{
       updation:
       if any ele is less than smallest
-      else if
-      
+      else if      
 
       if any ele is grater than great
       else if
 
      }
      first 2;
-     last 2;
-      
-
+     last 2;    
 
      return;
      
-
 Main{
     new;
     int arr[]
@@ -45,57 +38,56 @@ If you use Sort :O(nlogn); S.C:O(1)
      
 */
 
-
-
-
 import java.util.*;
-class arrimp{
-    public int getmax(int arr[]){
-        if(arr.length<2){
+
+class arrimp {
+    public long getmax(int arr[]) {
+        if (arr.length < 2) {
             return -1;
         }
-        if(arr.length==2){
-            return(arr[0]*arr[1]);
+        if (arr.length == 2) {
+            return (arr[0] * arr[1]);
         }
-        int smallest=arr[0]<arr[1]?arr[0]:arr[1];
-        int secsmallest=arr[0]>arr[1]?arr[0]:arr[1];
+        long smallest = arr[0] < arr[1] ? arr[0] : arr[1];
+        long secsmallest = arr[0] > arr[1] ? arr[0] : arr[1];
 
-        int greatest=arr[0]>arr[1]?arr[0]:arr[1];
-        int secgreatest=arr[0]<arr[1]?arr[0]:arr[1];
+        long greatest = arr[0] > arr[1] ? arr[0] : arr[1];
+        long secgreatest = arr[0] < arr[1] ? arr[0] : arr[1];
 
-        for(int i=2;i<arr.length;i++){
-            if(arr[i]<smallest){
-                smallest=secsmallest;
-                smallest=arr[i];
-            }else if(arr[i]<secsmallest){
-                secsmallest=arr[i];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] < smallest) {
+                secsmallest = smallest;
+                smallest = arr[i];
+            } else if (arr[i] < secsmallest) {
+                secsmallest = arr[i];
             }
-            if(arr[i]>greatest){
-                greatest=secgreatest;
-                greatest=arr[i];
-            }else if(arr[i]>secgreatest){
-                secgreatest=arr[i];
+            if (arr[i] > greatest) {
+                secgreatest = greatest;
+                greatest = arr[i];
+            } else if (arr[i] > secgreatest) {
+                secgreatest = arr[i];
             }
         }
-        int firsttwo=smallest*secsmallest;
-        int lasttwo=greatest*secgreatest;
-        return firsttwo>lasttwo? firsttwo:lasttwo;
+        long firsttwo = smallest * secsmallest;
+        long lasttwo = greatest * secgreatest;
+        return firsttwo > lasttwo ? firsttwo : lasttwo;
 
-    } 
-}
-public class MaxProductInUnsortedArray {
-    public static void main(String[] args) {
-    int num;
-    Scanner sc=new Scanner(System.in);
-    arrimp a=new arrimp();
-    int arr[]=new int[100];
-    num=sc.nextInt();
-
-    System.out.println("Enter the elements of the array: ");  
-    for(int k=0;k<num;k++){
-        arr[k]=sc.nextInt();
     }
-     
-    System.out.println(a.getmax(arr));
+}
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        arrimp a = new arrimp();
+        int arr[] = new int[100];
+        num = sc.nextInt();
+
+        for (int k = 0; k < num; k++) {
+            arr[k] = sc.nextInt();
+        }
+
+        System.out.println(a.getmax(arr));
+
     }
 }
