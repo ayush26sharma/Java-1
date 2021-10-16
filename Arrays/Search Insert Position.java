254f82dc2b235b368acb1db899c34cc3617e1581
AkshataABhat <74967139+AkshataABhat@users.noreply.github.com>
2021-10-16 22:29:45 +0530

    Update Search Insert Position.java

--- Arrays/Search Insert Position.java
+++ Arrays/Search Insert Position.java
@@ -2,19 +2,23 @@
 
 You must write an algorithm with O(log n) runtime complexity.*/
 
-
-
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left=1, right=nums.length-1;
-        while(left<right){
+        int left=0, right=nums.length-1;
+        while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]==target) return mid;
-            else if(nums[mid]>target)
+            if(nums[mid]==target){
+                 return mid;
+            }
+            else if(nums[mid]>target){
                 right=mid-1;
-            else left=mid+1;
-            
+            }
+            else {
+                left=mid+1;
+            }         
         } 
-        return left; 
+         return left;
     }
+            
+
 }
